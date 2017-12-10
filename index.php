168d879579c9e0450a76763d3f028c72cd180950
BasicPanda <jbl5277@gmail.com>
2017-12-10 13:29:11 -0500

    1.0.4

---
+++ index.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<!--LovelyBoutique Web Store by Jianbin Li-->
+<!--
+CSS
+---
+BOOTSTRAP
+CUSTOM
+-->
+<html lang="en">
+	<head>
+		<title>Lovely Boutique</title>
+		<meta charset="utf-8"/>
+		<meta name="viewport" content="width=device-width, initial-scale=1"/>
+		<meta name="Keywords" content=""/>
+
+		
+		<link rel="icon" href="/favicon.ico" type="image/x-icon"/>
+		<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-beta.2/css/bootstrap.min.css" integrity="sha384-PsH8R72JQ3SOdhVi3uxftmaW6Vc51MKb0q5P2rRUpPvrszuE4W1povHYgTpBfshb" crossorigin="anonymous">
+		<link rel="stylesheet" href="javascript/colors.css"/>
+	</head>
+	<body>
+	</body>
+</html>
